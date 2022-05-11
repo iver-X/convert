c5200ab76783849a8dd7ae85cf85aac60b0d852f
iver-X <105339997+iver-X@users.noreply.github.com>
2022-05-11 13:50:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 clear
-figlet Loading...
+figlet Loading...|lolcat
 pip install requests
 clear
-figlet Loading...
+figlet Loading...|lolcat
 pip install mechanize
 clear
-figlet Loading...
+figlet Loading...|lolcat
 pip install bs4
 clear
-figlet Loading...
+figlet Loading...|lolcat
 gem install lolcat
 clear
-figlet Loading...
+figlet Loading...|lolcat
 pip install uncompyle6
 clear
 figlet sukses|lolcat
 date|lolcat
+echo
