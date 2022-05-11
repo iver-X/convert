3c64c86963f43f200c3f8e72a656425b36f58beb
iver-X <105339997+iver-X@users.noreply.github.com>
2022-05-11 13:45:35 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+clear
+figlet Loading...
+pip install requests
+clear
+figlet Loading...
+pip install mechanize
+clear
+figlet Loading...
+pip install bs4
+clear
+figlet Loading...
+pip install uncompyle6
+clear
+figlet sukses
+date|
+Masukan perintah python Convert.py
