68f33fe8a36644647b0a20985b9d29908f70a658
Fanky <105339997+iver-X@users.noreply.github.com>
2022-05-12 12:46:17 +0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -79,12 +79,14 @@ class convert:
             'Cookie': cookie
         }
         response = r.get('https://web.facebook.com/adsmanager?_rdc=1&_rdr', headers = headers)
-      if re.search('(EAAA)\w+', str(response.text)):
+     find = re.findall('act=(.*?)&nav_source', respon.text)
         if len(find) == 0:
           masuk = input(f"""{MERAH}!.{HIJAU} Token tidak ditemukan
 {HIJAU}TEKAN {MERAH}ENTER!!""");os.system('python Convert.py')
         else:
-          for token in find:
+          for y in find:
+            response = r.get(f'https://web.facebook.com/adsmanager/manage/campaigns?act={y}&nav_source=no_referrer', headers = headers)
+            if re.search('(EAAA)\w+', str(response.text)):
             print(f"\n{KUNING}?.{PUTIH} Your token :{HIJAU} {token}")
     except Exception as e:
       exit(f"{MERAH}!.{MERAH} {e}")
