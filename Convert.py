25bc7a02b6cbb814a77063793b890fd3e1048eff
KAWAN <105339997+iver-X@users.noreply.github.com>
2022-05-12 20:31:12 +0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -39,7 +39,7 @@ class convert:
         self.__satu__(cookie)
       else:
         masuk = input(f'''{HIJAU}        JANGAN KOSONG GOBLOK!!!
-        {HIJAU}TEKAN {MERAH}ENTER!!''');os.system('python Convert.py');print()
+    {HIJAU}TEKAN {MERAH}ENTER!!''');os.system('python Convert.py');print()
     elif masuk == '2' or masuk == '02':
       cookie = input(f"\n{HIJAU}{U}    [!]{O}masukan Cookie :{KUNING} ")
       if 'c_user=' in str(cookie):
@@ -53,14 +53,14 @@ class convert:
         self.__tiga__(cookie)
       else:
         masuk = input(f'''{HIJAU}        JANGAN KOSONG GOBLOK!!!
-        {HIJAU}TEKAN {MERAH}ENTER!!''');os.system('python Convert.py');print()
+    {HIJAU}TEKAN {MERAH}ENTER!!''');os.system('python Convert.py');print()
     elif masuk == '4' or masuk == '04':
       print(f"{KUNING}[!!!]{HIJAU}          Anda akan diarahkan ke youtube...");time.sleep(2);os.system('xdg-open https://youtube.com/channel/UCTuJ142jrkZxS_QnkttTMNQ');os.system('python Convert.py')
     elif masuk == '5' or masuk == '05':
       exit('SELAMAT TINGGAL SAYANG')
     else:
       masuk = input(f'''{HIJAU}        JANGAN KOSONG GOBLOK!!!
-        {HIJAU}TEKAN {MERAH}ENTER!!''');os.system('python Convert.py')
+    {HIJAU}TEKAN {MERAH}ENTER!!''');os.system('python Convert.py')
   def __satu__(self,cookie):
     try:
       with requests.Session() as r:
@@ -71,8 +71,8 @@ class convert:
         response = r.get('https://web.facebook.com/ads/manager/account_settings/account_billing/?_rdc=1&_rdr', headers = headers)
         find = re.findall('(EAAI\w+)', response.text)
         if len(find) == 0:
-          masuk = input(f"""{MERAH}[×]{HIJAU} Token tidak ditemukan
-{HIJAU}TEKAN {MERAH}ENTER!!""");os.system('python Convert.py');print()
+          masuk = input(f"""    {MERAH}[×]{HIJAU} Token tidak ditemukan
+  {HIJAU}TEKAN {MERAH}ENTER!!""");os.system('python Convert.py');print()
         else:
           for token in find:
             print(f"\n{KUNING}{O} [✓]Token Kamu :{HIJAU} {token}")
@@ -88,8 +88,8 @@ class convert:
         response = r.get('https://web.facebook.com/adsmanager?_rdc=1&_rdr', headers = headers)
         find = re.findall('(EAAA)\w+', (response.text))
         if len(find) == 0:
-          masuk = input(f"""{MERAH}[×]{HIJAU} Token tidak ditemukan
-{HIJAU}TEKAN {MERAH}ENTER!!""");os.system('python Convert.py')
+          masuk = input(f"""    {MERAH}[×]{HIJAU} Token tidak ditemukan
+  {HIJAU}TEKAN {MERAH}ENTER!!""");os.system('python Convert.py')
         else:
           for token in find:
             print(f"\n{KUNING}{O}    [✓]Token Kamu :{HIJAU} {token}")
@@ -105,8 +105,8 @@ class convert:
         respon = r.get('https://web.facebook.com/adsmanager?_rdc=1&_rdr', headers = headers)
         find = re.findall('act=(.*?)&nav_source', respon.text)
         if len(find) == 0:
-          masuk = input(f"""{MERAH}[×]{HIJAU} Token tidak ditemukan
-{HIJAU}TEKAN {MERAH}ENTER!!""");os.system('python Convert.py');print()
+          masuk = input(f"""    {MERAH}[×]{HIJAU} Token tidak ditemukan
+  {HIJAU}TEKAN {MERAH}ENTER!!""");os.system('python Convert.py');print()
         else:
           for y in find:
             response = r.get(f'https://web.facebook.com/adsmanager/manage/campaigns?act={y}&nav_source=no_referrer', headers = headers)
