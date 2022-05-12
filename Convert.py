e59ca897f499c34431d964efdc4402ae8bd5a149
KAWAN <105339997+iver-X@users.noreply.github.com>
2022-05-12 20:59:12 +0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -20,11 +20,11 @@ def mengetik(s):
         sys.stdout.write(c)
         sys.stdout.flush()
 #kecepatan mengetik
-        time.sleep(random.random() * 0.1)
+        time.sleep(random.random() * 0.2)
 #ubah angka 0.1 sesuai keinginan kamu
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
-mengetik('hallo teman-teman w3wandroid\nselamat belajar dalam blog ilmu teknologi android\nterima kasih.')
+mengetik('SELAMAT DATANG\nPARA PECUNDANG.')
 #banner
 banner = (f"""
 {O}ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ╔═╗┌─┐┌┐┌┬  ┬┌─┐┬─┐┌┬┐
