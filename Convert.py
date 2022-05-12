b502f793695fc967fc1a2364f5d6ef75a7059042
KAWAN <105339997+iver-X@users.noreply.github.com>
2022-05-12 21:17:55 +0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -32,7 +32,7 @@ def mengetik(s):
 #ubah angka 0.1 sesuai keinginan kamu
 #angka terkecil adalah yang paling cepat
 #angka terbesar adalah yang paling lambat
-mengetik({H}'                 SELAMAT DATANG PARA PECUNDANG')
+{HIJAU}mengetik('                 SELAMAT DATANG PARA PECUNDANG')
 #lahha
 class convert:
 
@@ -133,4 +133,4 @@ class convert:
 
 
 if __name__=='__main__':
-  os.system('main');convert()
+  os.system('git pull');convert()
