865453426d7bd13bfa439c62d012d34c6b5572ea
Fanky <105339997+iver-X@users.noreply.github.com>
2022-05-12 13:59:40 +0700

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -23,8 +23,8 @@ class menu:
 banner()
 IP = requests.get('https://api.ipify.org').text
 jalan('[ selamat Datang Om  ]')
-jalan('IP')
-jalan('waktu')
+jalan('[*]ALAMAT IP KAMU SAAT INI:'IP)
+jalan('[*]WAKTU KAMU MASUK       :'waktu)
 print()
 # Convert Cookie Ke Token
 class convert:
